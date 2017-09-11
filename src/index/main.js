21010e25d1f2bc6030c1878590cbf4a58e15b6bd
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-09-11 22:26:03 +0200

    Require index param

---
+++ src/index/main.js
@@ -0,0 +1,7 @@
+var miss = require('mississippi')
+module.exports = (options) => {
+  if (!options.index){
+    throw new Error("Index not defined")
+  }
+
+}
--- test/index/main.js
+++ test/index/main.js
@@ -2,6 +2,15 @@ var expect = require('chai').expect
 var index = require('../../src/index/main')
 var StreamTest = require('streamtest')
 describe('index', () => {
+  it('Should fail if not passed index', (done) => {
+    try {
+      index({})
+      done(new Error("Should fail"))
+    } catch (error) {
+      expect(error).not.to.be.undefined
+      done()
+    }
+  })
   StreamTest.versions.forEach(function (version) {
     describe('for ' + version + ' streams', function () {
 
