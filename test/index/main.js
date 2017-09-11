74914eedd3ab0ca4f3eda22902ae1f2006c39be7
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-09-11 22:17:21 +0200

    Index module

---
+++ test/index/main.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect
+var index = require('../../src/index/main')
+var StreamTest = require('streamtest')
+describe('index', () => {
+  StreamTest.versions.forEach(function (version) {
+    describe('for ' + version + ' streams', function () {
+
+    })
+  })
+})
