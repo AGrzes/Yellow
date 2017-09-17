65d05a6ce2cea540641d23dcd0f3e3ce464736e7
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-09-17 22:07:00 +0200

    Corrected test group name

--- test/dump/json.js
+++ test/dump/json.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect
 var dumpJson = require('../../src/dump/json')
 var StreamTest = require('streamtest')
-describe('parse', () => {
+describe('dump', () => {
   describe('json', () => {
     StreamTest.versions.forEach(function (version) {
       describe('for ' + version + ' streams', function () {
