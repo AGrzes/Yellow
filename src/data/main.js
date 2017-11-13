a8e67840f4e01f0c9e87c18a8aca32d2f715d141
Grzegorz Abramczyk <abramczykg@gmail.com>
2017-11-13 21:58:40 +0100

    Lookup by sutertype

--- src/data/main.js
+++ src/data/main.js
@@ -1,8 +1,16 @@
 const _ = require('lodash')
 class Data {
-  constructor(model){
+  constructor(model,metadata){
     this.model = model
+    this.metadata = metadata
     this.byType = _(model).groupBy('type').value()
+    if (this.metadata){
+      this.byTypes = _(metadata.types).keyBy('name').mapValues((type) =>
+        _.flatMap(_.concat(type, type.descendants), (derivedType) =>
+          this.byType[derivedType.name] ? this.byType[derivedType.name] : []
+        )
+      ).value()
+    }
   }
 }
 exports.Data = Data
